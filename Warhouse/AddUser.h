ad0a81f2c7efaf07e555c519942f821f2f3ed96a
Aavin <aavinsidhu@hotmail.com>
2020-04-03 23:15:30 -0400

    made a change somewhere small

--- Warhouse/AddUser.h
+++ Warhouse/AddUser.h
@@ -16,7 +16,18 @@ namespace Warhouse {
 	{
 		//Variable declaration
 		int* thisGui = nullptr;
-		bool* thisXPushed = nullptr;
+	private: System::Windows::Forms::Label^ warehouseLabel;
+	private: System::Windows::Forms::ComboBox^ warehouseComboBox;
+	private: System::Windows::Forms::Button^ addButton;
+	private: System::Windows::Forms::Button^ backButton;
+	private: System::Windows::Forms::RadioButton^ adminAccessRadioButton;
+	private: System::Windows::Forms::RadioButton^ generalAccessRasioButton;
+	private: System::Windows::Forms::Label^ accessLevelLabel;
+	private: System::Windows::Forms::TextBox^ passwordTextBox;
+	private: System::Windows::Forms::TextBox^ usernameTextBox;
+	private: System::Windows::Forms::Label^ passwordLabel;
+	private: System::Windows::Forms::Label^ usernameLabel;
+		   bool* thisXPushed = nullptr;
 	public:
 		AddUser(void)
 		{
@@ -46,15 +57,15 @@ namespace Warhouse {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ usernameLabel;
-	private: System::Windows::Forms::Label^ passwordLabel;
-	private: System::Windows::Forms::TextBox^ usernameTextBox;
-	private: System::Windows::Forms::TextBox^ passwordTextBox;
-	private: System::Windows::Forms::Label^ accessLevelLabel;
-	private: System::Windows::Forms::RadioButton^ generalAccessRasioButton;
-	private: System::Windows::Forms::RadioButton^ adminAccessRadioButton;
-	private: System::Windows::Forms::Button^ backButton;
-	private: System::Windows::Forms::Button^ addButton;
+
+
+
+
+
+
+
+
+
 	protected:
 
 
@@ -75,106 +86,125 @@ namespace Warhouse {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->usernameLabel = (gcnew System::Windows::Forms::Label());
-			this->passwordLabel = (gcnew System::Windows::Forms::Label());
-			this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->passwordTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->accessLevelLabel = (gcnew System::Windows::Forms::Label());
-			this->generalAccessRasioButton = (gcnew System::Windows::Forms::RadioButton());
-			this->adminAccessRadioButton = (gcnew System::Windows::Forms::RadioButton());
-			this->backButton = (gcnew System::Windows::Forms::Button());
+			this->warehouseLabel = (gcnew System::Windows::Forms::Label());
+			this->warehouseComboBox = (gcnew System::Windows::Forms::ComboBox());
 			this->addButton = (gcnew System::Windows::Forms::Button());
+			this->backButton = (gcnew System::Windows::Forms::Button());
+			this->adminAccessRadioButton = (gcnew System::Windows::Forms::RadioButton());
+			this->generalAccessRasioButton = (gcnew System::Windows::Forms::RadioButton());
+			this->accessLevelLabel = (gcnew System::Windows::Forms::Label());
+			this->passwordTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->usernameTextBox = (gcnew System::Windows::Forms::TextBox());
+			this->passwordLabel = (gcnew System::Windows::Forms::Label());
+			this->usernameLabel = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
-			// usernameLabel
+			// warehouseLabel
 			// 
-			this->usernameLabel->AutoSize = true;
-			this->usernameLabel->Location = System::Drawing::Point(26, 30);
-			this->usernameLabel->Name = L"usernameLabel";
-			this->usernameLabel->Size = System::Drawing::Size(83, 20);
-			this->usernameLabel->TabIndex = 0;
-			this->usernameLabel->Text = L"Username";
+			this->warehouseLabel->AutoSize = true;
+			this->warehouseLabel->Location = System::Drawing::Point(26, 102);
+			this->warehouseLabel->Name = L"warehouseLabel";
+			this->warehouseLabel->Size = System::Drawing::Size(91, 20);
+			this->warehouseLabel->TabIndex = 21;
+			this->warehouseLabel->Text = L"Warehouse";
 			// 
-			// passwordLabel
+			// warehouseComboBox
 			// 
-			this->passwordLabel->AutoSize = true;
-			this->passwordLabel->Location = System::Drawing::Point(26, 93);
-			this->passwordLabel->Name = L"passwordLabel";
-			this->passwordLabel->Size = System::Drawing::Size(78, 20);
-			this->passwordLabel->TabIndex = 1;
-			this->passwordLabel->Text = L"Password";
+			this->warehouseComboBox->FormattingEnabled = true;
+			this->warehouseComboBox->Location = System::Drawing::Point(130, 94);
+			this->warehouseComboBox->Name = L"warehouseComboBox";
+			this->warehouseComboBox->Size = System::Drawing::Size(136, 28);
+			this->warehouseComboBox->TabIndex = 20;
 			// 
-			// usernameTextBox
+			// addButton
 			// 
-			this->usernameTextBox->Location = System::Drawing::Point(130, 30);
-			this->usernameTextBox->Name = L"usernameTextBox";
-			this->usernameTextBox->Size = System::Drawing::Size(136, 26);
-			this->usernameTextBox->TabIndex = 2;
+			this->addButton->Location = System::Drawing::Point(150, 200);
+			this->addButton->Name = L"addButton";
+			this->addButton->Size = System::Drawing::Size(116, 40);
+			this->addButton->TabIndex = 19;
+			this->addButton->Text = L"Add";
+			this->addButton->UseVisualStyleBackColor = true;
 			// 
-			// passwordTextBox
+			// backButton
 			// 
-			this->passwordTextBox->Location = System::Drawing::Point(130, 93);
-			this->passwordTextBox->Name = L"passwordTextBox";
-			this->passwordTextBox->PasswordChar = '*';
-			this->passwordTextBox->Size = System::Drawing::Size(136, 26);
-			this->passwordTextBox->TabIndex = 3;
+			this->backButton->Location = System::Drawing::Point(12, 200);
+			this->backButton->Name = L"backButton";
+			this->backButton->Size = System::Drawing::Size(116, 40);
+			this->backButton->TabIndex = 18;
+			this->backButton->Text = L"Back";
+			this->backButton->UseVisualStyleBackColor = true;
 			// 
-			// accessLevelLabel
+			// adminAccessRadioButton
 			// 
-			this->accessLevelLabel->AutoSize = true;
-			this->accessLevelLabel->Location = System::Drawing::Point(26, 162);
-			this->accessLevelLabel->Name = L"accessLevelLabel";
-			this->accessLevelLabel->Size = System::Drawing::Size(102, 20);
-			this->accessLevelLabel->TabIndex = 4;
-			this->accessLevelLabel->Text = L"Access Level";
+			this->adminAccessRadioButton->AutoSize = true;
+			this->adminAccessRadioButton->Location = System::Drawing::Point(134, 170);
+			this->adminAccessRadioButton->Name = L"adminAccessRadioButton";
+			this->adminAccessRadioButton->Size = System::Drawing::Size(79, 24);
+			this->adminAccessRadioButton->TabIndex = 17;
+			this->adminAccessRadioButton->Text = L"Admin";
+			this->adminAccessRadioButton->UseVisualStyleBackColor = true;
 			// 
 			// generalAccessRasioButton
 			// 
 			this->generalAccessRasioButton->AutoSize = true;
 			this->generalAccessRasioButton->Checked = true;
-			this->generalAccessRasioButton->Location = System::Drawing::Point(134, 149);
+			this->generalAccessRasioButton->Location = System::Drawing::Point(134, 140);
 			this->generalAccessRasioButton->Name = L"generalAccessRasioButton";
 			this->generalAccessRasioButton->Size = System::Drawing::Size(91, 24);
-			this->generalAccessRasioButton->TabIndex = 5;
+			this->generalAccessRasioButton->TabIndex = 16;
 			this->generalAccessRasioButton->TabStop = true;
 			this->generalAccessRasioButton->Text = L"General";
 			this->generalAccessRasioButton->UseVisualStyleBackColor = true;
 			// 
-			// adminAccessRadioButton
+			// accessLevelLabel
 			// 
-			this->adminAccessRadioButton->AutoSize = true;
-			this->adminAccessRadioButton->Location = System::Drawing::Point(134, 179);
-			this->adminAccessRadioButton->Name = L"adminAccessRadioButton";
-			this->adminAccessRadioButton->Size = System::Drawing::Size(79, 24);
-			this->adminAccessRadioButton->TabIndex = 6;
-			this->adminAccessRadioButton->Text = L"Admin";
-			this->adminAccessRadioButton->UseVisualStyleBackColor = true;
+			this->accessLevelLabel->AutoSize = true;
+			this->accessLevelLabel->Location = System::Drawing::Point(26, 153);
+			this->accessLevelLabel->Name = L"accessLevelLabel";
+			this->accessLevelLabel->Size = System::Drawing::Size(102, 20);
+			this->accessLevelLabel->TabIndex = 15;
+			this->accessLevelLabel->Text = L"Access Level";
 			// 
-			// backButton
+			// passwordTextBox
 			// 
-			this->backButton->Location = System::Drawing::Point(12, 209);
-			this->backButton->Name = L"backButton";
-			this->backButton->Size = System::Drawing::Size(116, 40);
-			this->backButton->TabIndex = 7;
-			this->backButton->Text = L"Back";
-			this->backButton->UseVisualStyleBackColor = true;
-			this->backButton->Click += gcnew System::EventHandler(this, &AddUser::backButton_Click);
+			this->passwordTextBox->Location = System::Drawing::Point(130, 53);
+			this->passwordTextBox->Name = L"passwordTextBox";
+			this->passwordTextBox->PasswordChar = '*';
+			this->passwordTextBox->Size = System::Drawing::Size(136, 26);
+			this->passwordTextBox->TabIndex = 14;
 			// 
-			// addButton
+			// usernameTextBox
 			// 
-			this->addButton->Location = System::Drawing::Point(150, 209);
-			this->addButton->Name = L"addButton";
-			this->addButton->Size = System::Drawing::Size(116, 40);
-			this->addButton->TabIndex = 8;
-			this->addButton->Text = L"Add";
-			this->addButton->UseVisualStyleBackColor = true;
-			this->addButton->Click += gcnew System::EventHandler(this, &AddUser::addButton_Click);
+			this->usernameTextBox->Location = System::Drawing::Point(130, 21);
+			this->usernameTextBox->Name = L"usernameTextBox";
+			this->usernameTextBox->Size = System::Drawing::Size(136, 26);
+			this->usernameTextBox->TabIndex = 13;
+			// 
+			// passwordLabel
+			// 
+			this->passwordLabel->AutoSize = true;
+			this->passwordLabel->Location = System::Drawing::Point(26, 53);
+			this->passwordLabel->Name = L"passwordLabel";
+			this->passwordLabel->Size = System::Drawing::Size(78, 20);
+			this->passwordLabel->TabIndex = 12;
+			this->passwordLabel->Text = L"Password";
+			// 
+			// usernameLabel
+			// 
+			this->usernameLabel->AutoSize = true;
+			this->usernameLabel->Location = System::Drawing::Point(26, 21);
+			this->usernameLabel->Name = L"usernameLabel";
+			this->usernameLabel->Size = System::Drawing::Size(83, 20);
+			this->usernameLabel->TabIndex = 11;
+			this->usernameLabel->Text = L"Username";
 			// 
 			// AddUser
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(278, 261);
+			this->Controls->Add(this->warehouseLabel);
+			this->Controls->Add(this->warehouseComboBox);
 			this->Controls->Add(this->addButton);
 			this->Controls->Add(this->backButton);
 			this->Controls->Add(this->adminAccessRadioButton);
@@ -212,6 +242,8 @@ private: System::Void AddUser_VisibleChanged(System::Object^ sender, System::Eve
 	//
 	//TODO: Add initalization code
 	//
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
